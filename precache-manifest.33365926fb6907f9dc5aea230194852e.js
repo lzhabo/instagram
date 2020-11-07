4aa10235d1bd5e78460659152844c227e9b199bc
Лидия <lidia@MacBook-Pro-Lidia.local>
2020-11-07 13:24:09 +0300

    Updates

---
+++ precache-manifest.33365926fb6907f9dc5aea230194852e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/instagram/assets/account.svg"
+  },
+  {
+    "revision": "88c822d612f04eb2550463a567c7f182",
+    "url": "/instagram/assets/camera.svg"
+  },
+  {
+    "revision": "4a4f020b3ae1bd43730d0c50a147fa73",
+    "url": "/instagram/assets/comment.svg"
+  },
+  {
+    "revision": "8688000c260f5a594d4f871e4a6bd67a",
+    "url": "/instagram/assets/direct.svg"
+  },
+  {
+    "revision": "005aa7b517bd5fdc7ab82b59b8b83d59",
+    "url": "/instagram/assets/favoriets.svg"
+  },
+  {
+    "revision": "074c3a06b7326b7b90664ae8d4dd579a",
+    "url": "/instagram/assets/gradient.svg"
+  },
+  {
+    "revision": "ca5da6e72e684f635cefbbde242b49f4",
+    "url": "/instagram/assets/home.svg"
+  },
+  {
+    "revision": "01d85b9b64c59860bf15c70b127f253a",
+    "url": "/instagram/assets/instLogo.svg"
+  },
+  {
+    "revision": "951c9e4db38bc29fe8871ca35cdb2456",
+    "url": "/instagram/assets/like.svg"
+  },
+  {
+    "revision": "64553869a7c305ea0ba1bce70c0d6f0b",
+    "url": "/instagram/assets/logo.svg"
+  },
+  {
+    "revision": "68d76883905682ca57f5258b81eca2ea",
+    "url": "/instagram/assets/menu.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/instagram/assets/newPhoto.svg"
+  },
+  {
+    "revision": "e6dca0bf5663700c890982c949517a4c",
+    "url": "/instagram/assets/post.svg"
+  },
+  {
+    "revision": "8d561a8f761761216e501a389e47c897",
+    "url": "/instagram/assets/search.svg"
+  },
+  {
+    "revision": "c568b45f55ebbe6140dcbaf56cff94a1",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "935d0296976484fe229d",
+    "url": "/instagram/static/css/main.3d497511.chunk.css"
+  },
+  {
+    "revision": "77842c05f8fbd7c067ed",
+    "url": "/instagram/static/js/2.6601a8ea.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/instagram/static/js/2.6601a8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935d0296976484fe229d",
+    "url": "/instagram/static/js/main.60659d39.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  },
+  {
+    "revision": "49d167fd00ec879e285af5661cc763aa",
+    "url": "/instagram/static/media/SFUIText-Bold.49d167fd.ttf"
+  },
+  {
+    "revision": "1e1d803a7a614c854ce213acf143edc2",
+    "url": "/instagram/static/media/SFUIText-Regular.1e1d803a.ttf"
+  },
+  {
+    "revision": "88c822d612f04eb2550463a567c7f182",
+    "url": "/instagram/static/media/camera.88c822d6.svg"
+  },
+  {
+    "revision": "4a4f020b3ae1bd43730d0c50a147fa73",
+    "url": "/instagram/static/media/comment.4a4f020b.svg"
+  },
+  {
+    "revision": "8688000c260f5a594d4f871e4a6bd67a",
+    "url": "/instagram/static/media/direct.8688000c.svg"
+  },
+  {
+    "revision": "005aa7b517bd5fdc7ab82b59b8b83d59",
+    "url": "/instagram/static/media/favoriets.005aa7b5.svg"
+  },
+  {
+    "revision": "074c3a06b7326b7b90664ae8d4dd579a",
+    "url": "/instagram/static/media/gradient.074c3a06.svg"
+  },
+  {
+    "revision": "ca5da6e72e684f635cefbbde242b49f4",
+    "url": "/instagram/static/media/home.ca5da6e7.svg"
+  },
+  {
+    "revision": "01d85b9b64c59860bf15c70b127f253a",
+    "url": "/instagram/static/media/instLogo.01d85b9b.svg"
+  },
+  {
+    "revision": "951c9e4db38bc29fe8871ca35cdb2456",
+    "url": "/instagram/static/media/like.951c9e4d.svg"
+  },
+  {
+    "revision": "64553869a7c305ea0ba1bce70c0d6f0b",
+    "url": "/instagram/static/media/logo.64553869.svg"
+  },
+  {
+    "revision": "68d76883905682ca57f5258b81eca2ea",
+    "url": "/instagram/static/media/menu.68d76883.svg"
+  },
+  {
+    "revision": "8d561a8f761761216e501a389e47c897",
+    "url": "/instagram/static/media/search.8d561a8f.svg"
+  }
+]);
